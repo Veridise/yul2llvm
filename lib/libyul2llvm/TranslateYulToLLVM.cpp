00564a593435654a266ae78590f09cd4c48bfb97
Ajinkya Rajput <ajinkya@veridise.com>
2022-08-08 12:06:00 +0530

    Touch base

--- lib/libyul2llvm/TranslateYulToLLVM.cpp
+++ lib/libyul2llvm/TranslateYulToLLVM.cpp
@@ -1,5 +1,15 @@
 #include "libyul2llvm/TranslateYulToLLVM.h"
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
+
+
+
 
 using namespace yul2llvm;
 
-void yul2llvm::TranslateYulToLLVM::run(const nlohmann::json &yulAst) {}
+void yul2llvm::TranslateYulToLLVM::run(const nlohmann::json &yulAst) {
+
+}
