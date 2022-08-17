0455d484ac1152a8e05fa724d42568421482d75d
elias julian marko garcia <elias@veridise.com>
2022-08-17 08:34:49 -0700

    Updated CLI. Removed unnecessary command. Streamline workflow.

--- src/pyul/cli.py
+++ src/pyul/cli.py
@@ -1,5 +1,6 @@
 """cli driver to configure and run pyul"""
 
+from operator import contains
 from random import randint
 import click
 
@@ -9,10 +10,10 @@ import os
 import pathlib
 import re
 import antlr4
-import json
-from utils.yul_parser import YulPrintListener, strip_all_trailing, make_json_arr
+from utils.yul_parser import YulPrintListener
 from utils.yul_translator import YulTranslator
 from utils.YulAntlr import YulLexer, YulParser
+from .core import inspect_json_ast
 
 
 @click.group()
@@ -42,28 +43,28 @@ def init():
 
 @run.command()
 @click.argument("_file", metavar="FILE")
-@click.option("--verbose", "-V", is_flag=True, default=False, help="Enable logging of results to stdout while running pyul.")
-@click.option("--pipeline", "-P", is_flag=True, default=False, help="Run translate and then parse on the output automatically.", )
-@click.pass_context
-def translate(ctx, _file, verbose, pipeline):
-    """transforms solc generated Yul code into more easily processed form. """
-    with open(_file, "r") as f:
-        raw_yul = f.read()
-
-    t = YulTranslator()
-    yul_str = t.translate(raw_yul)
-
-    new_file = _file + ".tmp"
-
-    with open(new_file, "w") as f:
-        f.write(yul_str)
-
-    if verbose:
-        click.echo(yul_str)
-
-    if pipeline:
-        click.echo(f"parsing flag pass, now parsing: {new_file}")
-        ctx.invoke(parse, _file=new_file)
+@click.option("--all-fns", is_flag=True, help="Emit all function defs")
+@click.option("--contract-fns", is_flag=True, help="Emit contract functions")
+@click.option("--unknown-names", is_flag=True, help="Emit unknown symbols")
+def inspect_ast(_file, all_fns, contract_fns, unknown_names):
+    """Extract program structure from a Yul AST JSON file. Prints all info by default."""
+    (named_fns, all_defs, unknown) = inspect_json_ast(_file)
+    if all_fns:
+        click.echo("\nAll function definitions:\n")
+        for fn in all_defs: click.echo(fn)
+    elif contract_fns:
+        click.echo("\nAll contract functions:\n")
+        for fn in named_fns: click.echo(fn)
+    elif unknown_names:
+        click.echo("\nAll unknown symbols and function calls:\n")
+        for fn in unknown: click.echo(fn)
+    else:
+        click.echo("\nAll function definitions:\n")
+        for fn in all_defs: click.echo(fn)
+        click.echo("\nAll contract functions:\n")
+        for fn in named_fns: click.echo(fn)
+        click.echo("\nAll unknown symbols and function calls:\n")
+        for fn in unknown: click.echo(fn)
 
 @run.command()
 @click.argument("_file", metavar="YUL_FILE")
@@ -71,10 +72,26 @@ def translate(ctx, _file, verbose, pipeline):
 @click.option("--dry-run", "-D", is_flag=True, help="Don't write results to file.")
 def parse(_file, verbose, dry_run):
     """parse a Yul file into its AST representation"""
-    input_stream = antlr4.FileStream(_file)
+
+    with open(_file, "r") as f:
+        raw_yul = f.read()
+
+    t = YulTranslator()
+    yul_str = t.translate(raw_yul)
+
+    file_prefix = os.path.splitext(_file)[0]
+    tmp_file = _file + ".tmp"
+
+    with open(tmp_file, "w") as f:
+        f.write(yul_str)
+
+    if verbose:
+        click.echo(yul_str)
+
+    input_stream = antlr4.FileStream(tmp_file)
 
     # step 1: make sure there's only one contract in one file (this file)
-    with open(_file, "r") as f:
+    with open(tmp_file, "r") as f:
         tmp_yul_str = f.read()
     nobj = re.findall(r"object \".*?\" \{", tmp_yul_str)
     if len(nobj) != 2:
@@ -93,58 +110,41 @@ def parse(_file, verbose, dry_run):
     parsed_yul = printer.built_string
 
     if not dry_run:
-        with open(_file + '.json', "w") as f:
+        with open(file_prefix + '.json', "w") as f:
             f.write(parsed_yul + "\n")
 
     if verbose:
-        # TODO: this is very messy. How I pipe this data to stdout needs to be cleaned up either with
-        # additional flags or by unifying output into a clean JSON format that keeps everything separate
-        # or a similarly minded approach. This will apply regardless of how we ultimately extract information
-        # such as the symbol table and extracted functions.
-        click.echo("Parsed Yul\n")
         click.echo(parsed_yul)
-        click.echo("\nKnown Symbols")
-        click.echo(printer.known_symbols)
-        click.echo("\nUnknown Symbols\n")
-        click.echo(printer.unknown_symbols.items())
-        click.echo("\nExtracted Functions\n")
-        click.echo(make_json_arr(printer.extracted_fn))
+
+
 
 @run.command()
 @click.argument("_file", metavar="FILE")
-@click.option("--input-type", "-iT", default="sol", type=str)
+@click.option("--verbose", "-V", is_flag=True, help="Enable logging of results to stdout while running pyul.")
 @click.option("--output", "-o", default="compiled", type=str, help="output directory")
-def compile(_file, input_type, output):
+def compile(_file, verbose, output):
     """compile sol file to yul"""
-    if input_type != "sol":
-        click.echo(".yul files are not supported yet.")
-        return
 
-    click.echo(f"Compiling {_file} to yul...")
+    if verbose: click.echo(f"Compiling {_file} to yul...")
 
     # get the output directories setup
     cwd = os.getcwd()
-    click.echo(f"cwd: {cwd}")
+    if verbose: click.echo(f"cwd: {cwd}")
     file_name = pathlib.Path(_file).stem
-    click.echo(f"file name: {file_name}")
+    if verbose: click.echo(f"file name: {file_name}")
 
     compile_dir = os.path.join(cwd, output)
 
-    result_id = randint(100, 100000)
-    result_dir = os.path.join(compile_dir, file_name + "_" + str(result_id))
+    result_dir = os.path.join(compile_dir, file_name)
     result_yul = os.path.join(result_dir, file_name + ".yul")
     result_storage = os.path.join(result_dir, file_name + "_storage")
     result_abi = os.path.join(result_dir, file_name + "_abi")
-    click.echo(f"output dir: {compile_dir}/")
-    click.echo(f"result dir: {result_dir}/")
+    if verbose: click.echo(f"output dir: {compile_dir}/")
+    if verbose: click.echo(f"result dir: {result_dir}/")
 
     if not os.path.exists(compile_dir):
         os.mkdir(compile_dir)
     if not os.path.exists(result_dir):
-        os.mkdir(result_dir)
-    else:
-        # wary of deleting output
-        result_dir = os.path.join(result_dir, str(randint(1,100)))
         os.mkdir(result_dir)
 
     # generate the yul
@@ -164,15 +164,41 @@ def compile(_file, input_type, output):
     # trim
     abi_spec = abi_output.split("\n", 3)[3]
 
-    click.echo("writing results to file...")
+    if verbose: click.echo("writing results to file...")
     with open(result_yul, "w") as f:
         f.writelines(yul_obj)
-        click.echo("yul successfully written")
+        if verbose: click.echo("yul successfully written")
     with open(result_storage, "w") as f:
         f.writelines(storage_layout)
-        click.echo("storage layout successfully written")
+        if verbose: click.echo("storage layout successfully written")
     with open(result_abi, "w") as f:
         f.writelines(abi_spec)
-        click.echo("abi specification successfully written")
+        if verbose: click.echo("abi specification successfully written")
 
-    click.echo("`pyul compile` finished running.")
+    if verbose: click.echo("`pyul compile` finished running.")
+
+
+@run.command()
+@click.argument("_f", metavar="FILE")
+@click.option("--verbose", "-V", is_flag=True, help="Enable logging of results to stdout while running pyul.")
+@click.option("--dry-run", "-D", is_flag=True, help="Don't write results to file.")
+@click.option("--output", "-o", default="compiled", type=str, help="output directory")
+@click.pass_context
+def all(ctx, _f, verbose, dry_run, output):
+    """runs all pyul commands in sequence for a given .sol file. """
+    # Grabbing path info we'd otherwise get from compile.
+    cwd = os.getcwd()
+    file_name = pathlib.Path(_f).stem
+    compile_dir = os.path.join(cwd, output)
+    result_dir = os.path.join(compile_dir, file_name)
+    yul_file = os.path.join(result_dir, file_name + ".yul")
+    json_file = os.path.join(result_dir, file_name + ".json")
+
+    if verbose: click.echo("Running `pyul compile`")
+    ctx.invoke(compile, _file=_f, verbose=verbose, output=output)
+
+    if verbose: click.echo("Running `pyul parse`")
+    ctx.invoke(parse, _file=yul_file, verbose=verbose, dry_run=dry_run)
+
+    if verbose: click.echo("Running `pyul inspect_ast`")
+    ctx.invoke(inspect_ast, _file=json_file)
