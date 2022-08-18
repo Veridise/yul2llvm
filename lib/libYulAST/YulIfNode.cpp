4219f6149d861243ec5e108cf09b1a48aa43dbd4
Ajinkya Rajput <ajinkya@veridise.com>
2022-08-18 17:05:07 +0530

    Yul implements if-then branches using 'yul-if' node and if-then-else branches using 'yul-switch' ndoes. This commit adds capability to implement if then branches. (There is a mistake in handling of return. A separate PR will be created for it)

--- lib/libYulAST/YulIfNode.cpp
+++ lib/libYulAST/YulIfNode.cpp
@@ -13,8 +13,9 @@ void YulIfNode::parseRawAST(const json *rawAST) {
 }
 
 YulIfNode::YulIfNode(const json *rawAST)
-    : YulStatementNode(rawAST,
-                       YUL_AST_STATEMENT_NODE_TYPE::YUL_AST_STATEMENT_IF) {
+    : YulStatementNode(
+          rawAST,
+          YUL_AST_STATEMENT_NODE_TYPE::YUL_AST_STATEMENT_IF) {
   assert(sanityCheckPassed(rawAST, YUL_IF_KEY));
   parseRawAST(rawAST);
 }
@@ -42,19 +43,25 @@ llvm::Value *YulIfNode::codegen(llvm::Function *enclosingFunction) {
   // create actual branch on condition
   Builder->CreateCondBr(cond, thenBlock, contBlock);
 
-  // emit then
+  //emit then 
   Builder->SetInsertPoint(thenBlock);
   thenBody->codegen(enclosingFunction);
   Builder->CreateBr(contBlock);
+  // thenBlock = Builder->GetInsertBlock();
 
   // merge node
   enclosingFunction->getBasicBlockList().push_back(contBlock);
   Builder->SetInsertPoint(contBlock);
   return nullptr;
+  // llvm::PHINode *phi = Builder->CreatePHI(llvm::Type::getInt32Ty(*TheContext), 2, "if-join");
+  // phi->addIncoming(thenBlock);
+  // phi->addIncoming(prevBlock);
 }
 
-std::unique_ptr<YulExpressionNode> &YulIfNode::getCondition() {
+std::unique_ptr<YulExpressionNode>& YulIfNode::getCondition() {
   return condition;
 }
 
-std::unique_ptr<YulBlockNode> &YulIfNode::getThenBody() { return thenBody; }
+std::unique_ptr<YulBlockNode>& YulIfNode::getThenBody() {
+  return thenBody;
+}
--- lib/libYulAST/YulNodeBuilder.cpp
+++ lib/libYulAST/YulNodeBuilder.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <libYulAST/YulNodeBuilder.h>
 
+
 #include <libYulAST/YulAssignmentNode.h>
+#include <libYulAST/YulLeaveNode.h>
+#include <libYulAST/YulVariableDeclarationNode.h>
 #include <libYulAST/YulBlockNode.h>
+#include <libYulAST/YulIfNode.h>
 #include <libYulAST/YulFunctionCallNode.h>
 #include <libYulAST/YulIdentifierNode.h>
-#include <libYulAST/YulIfNode.h>
-#include <libYulAST/YulLeaveNode.h>
 #include <libYulAST/YulNumberLiteralNode.h>
 #include <libYulAST/YulSwitchNode.h>
 #include <libYulAST/YulVariableDeclarationNode.h>
@@ -32,7 +34,7 @@ YulStatementBuilder::Builder(const json *rawAST) {
         std::make_unique<YulFunctionCallNode>(rawAST));
   } else if (!type.compare(YUL_IF_KEY)) {
     return std::unique_ptr<YulStatementNode>(
-        std::make_unique<YulIfNode>(rawAST));
+      std::make_unique<YulIfNode>(rawAST));
   } else if (!type.compare(YUL_BLOCK_KEY)) {
     return std::unique_ptr<YulStatementNode>(
         std::make_unique<YulBlockNode>(rawAST));
