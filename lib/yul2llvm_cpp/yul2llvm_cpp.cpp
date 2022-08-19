0458f44374f64b47c6aeb5c907922604bbc78192
Ajinkya Rajput <ajinkya@veridise.com>
2022-08-19 06:58:18 +0530

    clang formatted

--- lib/yul2llvm_cpp/yul2llvm_cpp.cpp
+++ lib/yul2llvm_cpp/yul2llvm_cpp.cpp
@@ -52,7 +52,23 @@ int main(int argc, char **argv) {
 
   yul2llvm::TranslateYulToLLVM translator(rawAST);
   translator.run();
-  translator.dumpFunctionsToFile(outputFile);
-  llvm::outs() << "llvm successfully generated";
+
+  std::error_code fileOpeningError;
+  if (outputFile == "-") {
+    translator.dumpFunctions(llvm::outs());
+    llvm::errs() << "llvm successfully generated\n";
+  } else {
+    llvm::raw_fd_ostream fstream(outputFile, fileOpeningError);
+    if (fileOpeningError) {
+      llvm::WithColor::error(llvm::errs())
+          << "Could not open output file for writing: "
+          << fileOpeningError.message() << "\n";
+      return EXIT_FAILURE;
+    } else {
+      translator.dumpFunctions(fstream);
+      llvm::errs() << "llvm successfully generated\n";
+    }
+  }
+
   return EXIT_SUCCESS;
 }
