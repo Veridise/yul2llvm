8be39dd13730676e43ad9bc4897450e5588a5635
Ajinkya Rajput <ajinkya@veridise.com>
2022-08-19 06:19:49 +0530

    formatted

--- lib/libYulAST/YulIfNode.cpp
+++ lib/libYulAST/YulIfNode.cpp
@@ -13,9 +13,8 @@ void YulIfNode::parseRawAST(const json *rawAST) {
 }
 
 YulIfNode::YulIfNode(const json *rawAST)
-    : YulStatementNode(
-          rawAST,
-          YUL_AST_STATEMENT_NODE_TYPE::YUL_AST_STATEMENT_IF) {
+    : YulStatementNode(rawAST,
+                       YUL_AST_STATEMENT_NODE_TYPE::YUL_AST_STATEMENT_IF) {
   assert(sanityCheckPassed(rawAST, YUL_IF_KEY));
   parseRawAST(rawAST);
 }
@@ -43,7 +42,7 @@ llvm::Value *YulIfNode::codegen(llvm::Function *enclosingFunction) {
   // create actual branch on condition
   Builder->CreateCondBr(cond, thenBlock, contBlock);
 
-  //emit then 
+  // emit then
   Builder->SetInsertPoint(thenBlock);
   thenBody->codegen(enclosingFunction);
   Builder->CreateBr(contBlock);
@@ -53,15 +52,14 @@ llvm::Value *YulIfNode::codegen(llvm::Function *enclosingFunction) {
   enclosingFunction->getBasicBlockList().push_back(contBlock);
   Builder->SetInsertPoint(contBlock);
   return nullptr;
-  // llvm::PHINode *phi = Builder->CreatePHI(llvm::Type::getInt32Ty(*TheContext), 2, "if-join");
+  // llvm::PHINode *phi =
+  // Builder->CreatePHI(llvm::Type::getInt32Ty(*TheContext), 2, "if-join");
   // phi->addIncoming(thenBlock);
   // phi->addIncoming(prevBlock);
 }
 
-std::unique_ptr<YulExpressionNode>& YulIfNode::getCondition() {
+std::unique_ptr<YulExpressionNode> &YulIfNode::getCondition() {
   return condition;
 }
 
-std::unique_ptr<YulBlockNode>& YulIfNode::getThenBody() {
-  return thenBody;
-}
+std::unique_ptr<YulBlockNode> &YulIfNode::getThenBody() { return thenBody; }
