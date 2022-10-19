12f0e8a0f328f031ee2cb3ca1fa1e743f0e2721a
veriAjinkya <110429948+veriAjinkya@users.noreply.github.com>
2022-10-20 04:13:49 +0530

    Fixed inheritance testcase

--- tests/e2e/feature/inheritance.sol
+++ tests/e2e/feature/inheritance.sol
@@ -7,14 +7,14 @@ pragma solidity ^0.8.10;
 
 contract Base{
     uint256 baseMember;
-    function baseFunction() external view return(uint256){
+    function baseFunction() external view returns(uint256){
         return baseMember;
     }
 }
 
-contract Derived is base {
+contract Derived is Base {
     uint256 derivedMember;
-    function derivedFunction() external view return(uint256){
+    function derivedFunction() external view returns(uint256){
         return derivedMember;
     }
 }
